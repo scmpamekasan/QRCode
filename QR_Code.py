f8a62423694fcfdcafadc93efa1b24b9cc513333
scmpamekasan <109006899+scmpamekasan@users.noreply.github.com>
2024-03-12 12:48:26 +0700

    Update QR_Code.py

--- QR_Code.py
+++ QR_Code.py
@@ -14,37 +14,35 @@ def load_image(img):
     im = Image.open(img)
     return im
 
+@st.cache
+def load_data():
+    return pd.read_excel(
+        io="List_QR.xlsx",
+        engine="openpyxl",
+        sheet_name="Customer",
+        usecols=["OutletID", "YourColumnName"],  # Adjust column names as needed
+        nrows=20000,
+    )
+
 st.subheader("Create QR Code")
 with st.form(key='myqr_form'):
     raw_text = st.text_area("Input Kode Outlet disini (Kode Huruf Menggunakan Huruf Kapital)", max_chars=8)
     submit_button = st.form_submit_button("Generate")
-    df = pd.read_excel(
-        io ="List_QR.xlsx",
-        engine="openpyxl",
-        sheet_name="Customer",
-        usecols="B:H",
-        nrows=20000,
-    ) 
+    df = load_data()
 
-    # df1 = df.drop(['Nama_Outlet','Alamat','Zona','Sektor','Tgl Process'], axis=1)
     df1 = df.drop(['Alamat','Zona','Sektor','Tgl Process'], axis=1)
 
     df2 = df1[df1["OutletID"].str.contains(raw_text)]
-    # df4 = df2.iloc[0][1]
     df3 = df2.iloc[0].iloc[2]
     df4 = df2.iloc[0].iloc[1]
 
-# Correct indentation for the following block
 if submit_button:
     col1, col2 = st.columns(2)
     with col1:
-        # Add Data
         qr.add_data(df3)
-        # Generate
         qr.make(fit=True)
         img = qr.make_image(fill_color='black', back_color='white')
 
-        # Filename
         img_filename = "{}.png".format(raw_text)
         path_for_images = os.path.join(img_filename)
         img.save(path_for_images)
