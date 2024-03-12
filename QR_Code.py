5ef83f418026dd0dac85aad9a39c5246916d18e2
scmpamekasan <109006899+scmpamekasan@users.noreply.github.com>
2024-03-12 13:21:58 +0700

    Update QR_Code.py

--- QR_Code.py
+++ QR_Code.py
@@ -25,43 +25,40 @@ def load_data():
 
 st.subheader("Create QR Code")
 with st.form(key='myqr_form'):
-	raw_text = st.text_area("Input Kode Outlet disini (Kode Huruf Menggunakan Huruf Kapital)", max_chars=8)
-	submit_button = st.form_submit_button("Generate")
-	df = pd.read_excel(
-    		io ="List_QR.xlsx",
-    		engine="openpyxl",
-    		sheet_name="Customer",
-    		usecols="B:H",
-    		nrows=20000,
-    		) 
+    raw_text = st.text_area("Input Kode Outlet disini (Kode Huruf Menggunakan Huruf Kapital)", max_chars=8)
+    submit_button = st.form_submit_button("Generate")
+    df = pd.read_excel(
+        io="List_QR.xlsx",
+        engine="openpyxl",
+        sheet_name="Customer",
+        usecols="B:H",
+        nrows=20000,
+    )
 
-	df1 = df.drop(['Alamat','Zona','Sektor','Tgl Process'], axis=1)
+    df1 = df.drop(['Alamat','Zona','Sektor','Tgl Process'], axis=1)
 
+    df2 = df1[df1["OutletID"].str.contains(raw_text)]
 
-	df2 = df1[df1["OutletID"].str.contains(raw_text)]
-	df3 = df2.iloc[0][2]
-	df4 = df2.iloc[0][1]
+if submit_button:
+    col1, col2 = st.columns(2)
+    with col1:
+        if not df2.empty:
+            df3 = df2.iloc[0].iloc[2]
+            qr.add_data(df3)
+            qr.make(fit=True)
+            img = qr.make_image(fill_color='black', back_color='white')
 
-	
-	#st.write(df3)
+            img_filename = "{}.png".format(raw_text)
+            path_for_images = os.path.join(img_filename)
+            img.save(path_for_images)
 
-if submit_button :
-	col1, col2 = st.columns(2)
-	with col1:
-		# Add Data
-		qr.add_data(df3)
-		# Generate
-		qr.make(fit=True)
-		img = qr.make_image(fill_color='black', back_color='white')
+            final_img = load_image(path_for_images)
+            st.image(final_img)
 
-		# Filename
-		img_filename = "{}.png".format(raw_text)
-		path_for_images = os.path.join(img_filename)
-		img.save(path_for_images)
-
-		final_img = load_image(path_for_images)
-		st.image(final_img)
-
-	with col2:
-		st.info('Nama Toko')
-		st.write(df4)
+    with col2:
+        if not df2.empty:
+            st.info('Nama Toko')
+            df4 = df2.iloc[0].iloc[1]
+            st.write(df4)
+        else:
+            st.warning("No matching records found.")
